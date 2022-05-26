e6bba340c9ddd52b04678a7bdfc9870c0983eb39
rashminainwal01 <rashmi.nainwal@kiwitech.com>
2022-05-26 15:47:43 +0530

    modification in templates

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
 
     path('', views.sender.as_view(), name="sender"),
-
 ]
--- app/views.py
+++ app/views.py
@@ -13,8 +13,6 @@ class sender(View):
         email_from = request.POST.get('email_from')
         email_to = request.POST.get('email_to')
         host = request.POST.get('host')
-        # UseSecuredConnection = request.POST.get('UseSecuredConnection')
-        # Useauthentication = request.POST.get('Useauthentication')
 
         ''' Email message'''
         import smtplib
@@ -25,26 +23,19 @@ class sender(View):
         port = port
         login = login
         host = host
-        # UseSecuredConnection = UseSecuredConnection
-        # Useauthentication = Useauthentication
 
         # creates SMTP session
         s = smtplib.SMTP(host, port)
-        # s = smtplib.SMTP('smtp.gmail.com', port)
+
         s.ehlo()
         # start TLS for security
         s.starttls()
         s.ehlo()
-        #
-        # if UseSecuredConnection == true:
-        #     if Useauthentication == true:
 
         # Authentication
         s.login(sender_mail, sender_mail_pass)
-        # else:
-        #     return "error"
-        # message to be sent
 
+        # message to be sent
         subject = 'SMTP email test'
         message = 'Subject: {}\n\n{}'.format(subject, 'mail is working fine')
 
